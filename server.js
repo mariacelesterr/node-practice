6a126898d3a513abbaf1f146328c5005c34bc57f
Maria Celeste <maria_rr_13@hotmail.com>
2021-07-21 16:36:17 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/',(req,res) => {
+    res.send('Hello work');
+})
+
+app.listen(port, () => {
+    console.log(` App is listening at http://localhost:${port} `);
+})
